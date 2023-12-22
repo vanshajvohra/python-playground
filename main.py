5016dc02681fb2e9052b112d7293b37abab9a856
Vanshaj Vohra <vanshajvohra@gmail.com>
2023-12-22 13:28:15 +0530

    Syntax Revision v1

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("chomu" in "Hackathon")
+print('chomu'.find('c'))
+is_hot = True
+name = "yippee"
+if is_hot: print(name)
+print('e' in name)
+print(name.find('p'))
+print(len(name))
